7116979965929c9277900fc90789da823c80f8d9
Lucid <29291906+IndecisiveBoolean@users.noreply.github.com>
2018-06-17 14:15:29 -0400

    add example items to the TODO List and make the list scrollable.

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,73 @@
-const addBtn = document.querySelector('.add_item_button');
-const clearBtn = document.querySelector('.clear_list_button');
-const addItemInput = document.querySelector('.add-item-input-field');
-const ul = document.querySelector('.ul_for_list_gen');
-let li = document.createElement('li');
+const incompleteListDiv = document.querySelector('.listItem');
+const addItemInput = document.querySelector('input.addItemInputField');
+const listItemContainer = document.querySelector('div.listItemContainer');
+
+const addItemButton = document.querySelector('.addItemButton');
+const clearListButton = document.querySelector('.clearListButton');
 
 
-addBtn.addEventListener('click', () => {
-	let ul = document.querySelector('.ul_for_list_gen');
-  let li = document.createElement('li');
-  li.className += "listItem";
-  li.textContent = addItemInput.value;
-  ul.appendChild(li);
-  addItemInput.value = '';
+
+listItemContainer.addEventListener('click', (event) => {
+// checks to see what button was clicked on the generated LI in the UL
+  if (event.target.tagName == 'BUTTON') {
+    console.log("delete button was clicked");
+    let button = event.target;
+    let li = button.parentNode;
+    let ul = li.parentNode;
+    ul.removeChild(li);
+  } else if (event.target.tagName == 'INPUT') {
+    console.log("checkbox was clicked");
+    let checkedItem = event.target;
+    let li = checkedItem.parentNode;
+    
+    if (checkedItem.checked) {
+        checkedItem.checked = true;
+        li.style.textDecoration = 'line-through';
+        li.style.color = 'lightgrey';
+        // moveCompletedTask(checkedItem);
+        
+        } else {
+          li.style.textDecoration = 'none';
+          li.style.color = 'black';
+        }
+  }
 });
 
-clearBtn.addEventListener('click', () => {
-	let ul = document.querySelector('.ul_for_list_gen');
+//generates the Add Item, Clear List, Checkbox, and DELETE buttons and creates the LI elements using the Add Item button and checks the value of the INPUT field. If field is empty an alert is triggered.
+addItemButton.addEventListener('click', () => {
+  let ul = document.querySelectorAll('ul')[0];
+  if (addItemInput.value === '') {
+      alert("Please specify a task to be added to the list.");
+      } else {
+          if (document.querySelector('div.examplesContainer')) {
+            document.querySelector('div.examplesContainer').style.display = "none";
+          }
+          let li = document.createElement('li');
+          let removeItemButton = document.createElement('button');
+          let checkBox = document.createElement('input');
+          removeItemButton.className += "removeItemButton";
+          removeItemButton.textContent = "DELETE";
+          checkBox.type = "checkbox";
+          checkBox.name = "markComplete";
+          checkBox.className = "checkBox";
+          checkBox.value = "unchecked";
+          li.className += "listItem";
+          li.textContent = addItemInput.value;
+          ul.appendChild(li);
+          li.appendChild(removeItemButton);
+          li.appendChild(checkBox);
+          addItemInput.value = '';
+      }
+});
+
+//Deletes all content generated within the UL's
+clearListButton.addEventListener('click', () => {
+  let ul = document.querySelectorAll('ul')[0];
   let li = document.querySelector('li');
-  	if (li.parentNode) {
+  	if (ul.parentNode) {
     	while ( ul.firstChild ) {
   			ul.removeChild( ul.firstChild );
 			}
     } else {}
 });
-
-
-
-
-
-
-
-
-
-
-
 
